5937ad95bccac07925c18e2de2f2ec484237ce63
isabelly gomes <isabellygom.veleiros@gmail.com>
2025-05-08 16:00:36 -0300

    Update JS

--- jogo-cobrinha/script.js
+++ jogo-cobrinha/script.js
@@ -41,3 +41,53 @@ const handleGameOver = () => {
     alert("Game Over! Aperte OK para recomeçar :)")
     location.reload();
 }
+
+/* Função para mudar a direção da cobrinha */
+const changeDirection = e => {
+    if (e.key === "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.key === "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.key === "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (e.key === "ArrowRigth" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    } 
+}
+
+controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.CDATA_SECTION_NODE.key})));
+
+/* Começar o game = init game */
+const initGame = () => {
+    if (gameOver) return handleGameOver();
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"`;
+
+    //Quando a cobra se alimenta - When snake eat food
+    if (snakeX === foodX && snakeY === foodY) {
+        updateFoodPosition();
+        snakeBody.push([foodY, foodX]);
+        score++;
+        highScore = score >= highScore ? score : highScore
+
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerHTML = `Score: ${score}`;
+        highScoreElement.innerHTML = `High Score: ${highScore}`;
+    }
+
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+
+    snakeBody[0] = [snakeX, snakeY];
+
+    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+        return gameOver = true;
+    }
+}
