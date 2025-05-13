a49abb4a67d33ff6798900c2eb7618b9c0b10818
isabelly gomes <isabellygom.veleiros@gmail.com>
2025-05-13 15:13:42 -0300

    Update concluído

--- script.js
+++ script.js
@@ -47,24 +47,27 @@ const changeDirection = e => {
     if (e.key === "ArrowUp" && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
+
     } else if (e.key === "ArrowDown" && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
+
     } else if (e.key === "ArrowLeft" && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
-    } else if (e.key === "ArrowRigth" && velocityX != -1) {
+
+    } else if (e.key === "ArrowRight" && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     } 
 }
 
-controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.CDATA_SECTION_NODE.key})));
+controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
 
 /* Começar o game = init game */
 const initGame = () => {
     if (gameOver) return handleGameOver();
-    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"`;
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
     //Quando a cobra se alimenta - When snake eat food
     if (snakeX === foodX && snakeY === foodY) {
@@ -90,4 +93,20 @@ const initGame = () => {
     if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
         return gameOver = true;
     }
+
+    //add div para cada parte do corpo da cobra
+    for (let i = 0; i < snakeBody.length; i++ ) {
+        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`; //pra cada movimento 1 quadradado
+        //verifica se a cabeça  da cobra atingiu ou colidiu com o corpo
+        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true;
+        }
+        playBoard.innerHTML = html;
+    }
+
 }
+
+updateFoodPosition();
+setIntervalId = setInterval(initGame, 100);
+document.addEventListener("keyup", changeDirection);
+
